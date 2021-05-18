1a3082d6c4e2fbbf15c665898bce6ea70d1b5d22
Jakob K <bluefireolymp@gmail.com>
2021-05-18 18:13:36 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.5.0"
+}
+
+group = "net.axay"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
