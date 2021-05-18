2ed24b8ff017312b3d29374c964952da79ae51db
Jakob K <bluefireolymp@gmail.com>
2021-05-18 22:30:03 +0200

    Add kit player events

--- src/main/kotlin/net/axay/kotlinkitapi/api/Kit.kt
+++ src/main/kotlin/net/axay/kotlinkitapi/api/Kit.kt
@@ -1,10 +1,12 @@
 package net.axay.kotlinkitapi.api
 
 import net.axay.kotlinkitapi.builder.KitBuilder
+import org.bukkit.event.Listener
 
 open class Kit internal constructor(
     val name: String,
     val items: Collection<KitItem>,
+    val kitPlayerEvents: Collection<Listener>,
 ) {
     companion object {
         inline operator fun invoke(name: String, builder: KitBuilder.() -> Unit) =
--- src/main/kotlin/net/axay/kotlinkitapi/builder/KitBuilder.kt
+++ src/main/kotlin/net/axay/kotlinkitapi/builder/KitBuilder.kt
@@ -6,20 +6,36 @@ import net.axay.kotlinkitapi.api.ClickableKitItem
 import net.axay.kotlinkitapi.api.HoldableKitItem
 import net.axay.kotlinkitapi.api.Kit
 import net.axay.kotlinkitapi.api.KitItem
+import net.axay.kspigot.event.listen
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerEvent
 import org.bukkit.inventory.ItemStack
 
 class KitBuilder(
     val name: String,
 ) {
-    private val items = ArrayList<KitItem>()
+    inner class Internal {
+        val items = ArrayList<KitItem>()
 
-    fun build() = Kit(name, items)
+        val kitPlayerEvents = ArrayList<Listener>()
+
+        fun build() = Kit(name, items, kitPlayerEvents)
+    }
+
+    val internalBuilder = this.Internal()
 
     fun clickableItem(stack: ItemStack, cooldown: Long, onClick: () -> Unit) {
-        items += ClickableKitItem(stack, cooldown, onClick)
+        internalBuilder.items += ClickableKitItem(stack, cooldown, onClick)
     }
 
     fun holdableItem(stack: ItemStack, period: Long, onHold: () -> Unit) {
-        items += HoldableKitItem(stack, period, onHold)
+        internalBuilder.items += HoldableKitItem(stack, period, onHold)
+    }
+
+    inline fun <reified T : PlayerEvent> kitPlayerEvent(crossinline callback: (T) -> Unit) {
+        internalBuilder.kitPlayerEvents += listen<T>(register = false) {
+            // TODO if player kit == this kit
+            callback.invoke(it)
+        }
     }
 }
