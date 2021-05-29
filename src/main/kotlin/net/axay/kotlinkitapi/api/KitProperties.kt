3687f48d315fe16ecea661b47414b66d064df087
Jakob K <bluefireolymp@gmail.com>
2021-05-30 00:03:04 +0200

    Use hocon for kit config

--- src/main/kotlin/net/axay/kotlinkitapi/api/KitProperties.kt
+++ src/main/kotlin/net/axay/kotlinkitapi/api/KitProperties.kt
@@ -1,83 +1,56 @@
+@file:Suppress("MemberVisibilityCanBePrivate", "unused")
+
 package net.axay.kotlinkitapi.api
 
-import kotlinx.serialization.json.*
-import net.axay.kotlinkitapi.config.Config
+import io.github.config4k.extract
+import net.axay.kotlinkitapi.config.ConfigManager
+import net.axay.kspigot.extensions.bukkit.warn
+import net.axay.kspigot.extensions.server
 import kotlin.reflect.KProperty
 
 abstract class KitProperties(val kitname: String) {
     abstract class KitProperty<T> {
         abstract val defaultValue: T
 
-        private var kitName: String? = null
-        private var propertyName: String? = null
+        protected var kitName: String? = null
+        protected var propertyName: String? = null
 
-        private var value: T? = null
+        protected var value: T? = null
 
-        operator fun getValue(thisRef: Any, property: KProperty<*>): T {
-            if (kitName == null) kitName = (thisRef as KitProperties).kitname
-            if (propertyName == null) propertyName = property.name
-
-            val currentValue = value
-
-            return if (currentValue == null) {
-                val jsonElement = Config.kits[kitName!!]?.jsonObject?.get(propertyName!!)
-                val newValue = if (jsonElement != null) {
-                    parseJsonElement(jsonElement) ?: kotlin.run {
-                        println("Could not parse given property $propertyName of kit $kitName")
-                        defaultValue
-                    }
-                } else defaultValue
-                return newValue.also { value = it }
-            } else currentValue
-        }
+        abstract operator fun getValue(thisRef: Any, property: KProperty<*>): T
 
         operator fun setValue(thisRef: Any?, property: KProperty<*>, newValue: T) {
             value = newValue
         }
-
-        protected abstract fun parseJsonElement(jsonElement: JsonElement): T?
     }
-
-    class BooleanProperty(override val defaultValue: Boolean) : KitProperty<Boolean>() {
-        override fun parseJsonElement(jsonElement: JsonElement) = jsonElement.jsonPrimitive.booleanOrNull
-    }
-
-    class IntProperty(override val defaultValue: Int) : KitProperty<Int>() {
-        override fun parseJsonElement(jsonElement: JsonElement) = jsonElement.jsonPrimitive.intOrNull
-    }
-
-    class LongProperty(override val defaultValue: Long) : KitProperty<Long>() {
-        override fun parseJsonElement(jsonElement: JsonElement) = jsonElement.jsonPrimitive.longOrNull
-    }
-
-    class FloatProperty(override val defaultValue: Float) : KitProperty<Float>() {
-        override fun parseJsonElement(jsonElement: JsonElement) = jsonElement.jsonPrimitive.floatOrNull
-    }
-
-    class DoubleProperty(override val defaultValue: Double) : KitProperty<Double>() {
-        override fun parseJsonElement(jsonElement: JsonElement) = jsonElement.jsonPrimitive.doubleOrNull
-    }
-
-    class StringProperty(override val defaultValue: String) : KitProperty<String>() {
-        override fun parseJsonElement(jsonElement: JsonElement) = jsonElement.jsonPrimitive.let {
-            if (it.isString) it.toString() else null
-        }
-    }
-
-    fun boolean(default: Boolean) = BooleanProperty(default)
-
-    fun int(default: Int) = IntProperty(default)
-
-    fun long(default: Long) = LongProperty(default)
-
-    fun float(default: Float) = FloatProperty(default)
-
-    fun double(default: Double) = DoubleProperty(default)
-
-    fun string(default: String) = StringProperty(default)
 
     inline fun <reified T : Any> any(default: T) = object : KitProperty<T>() {
         override val defaultValue = default
-        override fun parseJsonElement(jsonElement: JsonElement): T? = Json.decodeFromJsonElement(jsonElement)
+
+        override fun getValue(thisRef: Any, property: KProperty<*>): T {
+            if (kitName == null)
+                kitName = (thisRef as? KitProperties)?.kitname
+                    ?: error("Using a kit property outside of a KitProperties class")
+            if (propertyName == null)
+                propertyName = property.name
+
+            val currentValue = value
+            if (currentValue != null) return currentValue
+
+            val newValue = try {
+                ConfigManager.kits.getConfig(kitName!!).extract<T>(propertyName!!)
+            } catch (exc: Exception) {
+                server.consoleSender.warn("The property '$propertyName' of kit '$kitName' is given, but it could not be parsed! Using default value as a fallback.")
+                null
+            } ?: defaultValue
+            return newValue.also { value = it }
+        }
     }
+
+    fun boolean(default: Boolean) = any(default)
+    fun int(default: Int) = any(default)
+    fun long(default: Long) = any(default)
+    fun float(default: Float) = any(default)
+    fun double(default: Double) = any(default)
+    fun string(default: String) = any(default)
 }
---
+++ src/main/kotlin/net/axay/kotlinkitapi/config/ConfigManager.kt
@@ -0,0 +1,12 @@
+package net.axay.kotlinkitapi.config
+
+import com.typesafe.config.Config
+import com.typesafe.config.ConfigFactory
+import net.axay.kspigot.config.PluginFile
+
+object ConfigManager {
+    val kits: Config by lazy {
+        val configString = PluginFile("kits.json").readText()
+        ConfigFactory.parseString(configString)
+    }
+}
