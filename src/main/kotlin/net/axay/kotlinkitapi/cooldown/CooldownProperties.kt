cb73877df98af2d28d65e3c6ab05e1d02cd0d50d
Jakob K <bluefireolymp@gmail.com>
2021-05-30 16:08:07 +0200

    Add CooldownProperties for KitProperties

---
+++ src/main/kotlin/net/axay/kotlinkitapi/cooldown/CooldownProperties.kt
@@ -0,0 +1,19 @@
+package net.axay.kotlinkitapi.cooldown
+
+import net.axay.kotlinkitapi.api.KitProperties
+import net.axay.kotlinkitapi.builder.KitBuilder
+import org.bukkit.entity.Player
+import org.bukkit.event.player.PlayerEvent
+
+abstract class CooldownProperties(default: Long) : KitProperties() {
+    val cooldown by long(default)
+
+    @Suppress("LeakingThis")
+    val cooldownInstance = Cooldown(this::cooldown)
+}
+
+fun <P : CooldownProperties> KitBuilder<P>.applyCooldown(player: Player, block: CooldownScope.() -> Unit) =
+    player.applyCooldown(kit.properties.cooldownInstance, block)
+
+fun <P : CooldownProperties> KitBuilder<P>.applyCooldown(event: PlayerEvent, block: CooldownScope.() -> Unit) =
+    event.applyCooldown(kit.properties.cooldownInstance, block)
